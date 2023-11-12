08bef6097b5ba9997bec03a10bb70d94da47cf85
msfukui <msfukui@gmail.com>
2023-11-12 22:18:43 +0900

    Dollarクラスの乗法のテストで連続して実行した場合に失敗するテストを追加

--- tests/Unit/DollarTest.php
+++ tests/Unit/DollarTest.php
@@ -9,5 +9,7 @@ describe("Dollar", function () {
         $five = new Dollar(5);
         $five->times(2);
         expect($five->amount)->toBe(10);
+        $five->times(3);
+        expect($five->amount)->toBe(15);
     });
 });
