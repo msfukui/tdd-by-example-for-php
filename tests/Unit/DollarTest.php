6363f02494785e86c2d07fdd2b411bb5789346fa
msfukui <msfukui@gmail.com>
2023-11-14 17:06:33 +0900

    equals のテストとコードを実装

--- tests/Unit/DollarTest.php
+++ tests/Unit/DollarTest.php
@@ -5,11 +5,17 @@ declare (strict_types=1);
 namespace TddByExampleForPhp;
 
 describe("Dollar", function () {
+
     test("multiplication", function () {
         $five = new Dollar(5);
         $product = $five->times(2);
         expect($product->amount)->toBe(10);
         $product = $five->times(3);
         expect($product->amount)->toBe(15);
+    });
+
+    test("equality", function () {
+        expect((new Dollar(5))->equals(new Dollar(5)))->toBeTrue;
+        expect((new Dollar(5))->equals(new Dollar(6)))->toBeFalse;
     });
 });
