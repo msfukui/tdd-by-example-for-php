a9b3759e1ebdd5f886c52c8f6aa673e70ce46cb6
msfukui <msfukui@gmail.com>
2023-12-24 09:26:44 +0900

    第18章の TestCaseTest の実装を誤っていた点を修正

--- src/PartTwo/TestCaseTest/TestCaseTest.php
+++ src/PartTwo/TestCaseTest/TestCaseTest.php
@@ -7,10 +7,11 @@ namespace TddByExampleForPhp\PartTwo\TestCaseTest;
 require_once('../../../vendor/autoload.php');
 
 use TddByExampleForPhp\PartTwo\WasRun;
+use TddByExampleForPhp\PartTwo\TestCase;
 
-final class TestCaseTest
+final class TestCaseTest extends TestCase
 {
-    public static function testRunning(): void
+    public function testRunning(): void
     {
         $test = new WasRun("testMethod");
         assert($test->wasRun === false);
@@ -19,4 +20,5 @@ final class TestCaseTest
     }
 }
 
-TestCaseTest::testRunning();
+$testCaseTest = new TestCaseTest("testRunning");
+$testCaseTest->run();
